43f2d55e050a01b4927546bee1a58ceac058c67b
mdomasevicius <mdomasevicius@gmail.com>
2018-07-11 22:09:03 +0300

    Remove unpaid orders

--- src/main/java/mdomasevicius/escrow/orders/Orders.java
+++ src/main/java/mdomasevicius/escrow/orders/Orders.java
@@ -1,9 +1,14 @@
 package mdomasevicius.escrow.orders;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.time.LocalDateTime.now;
+import static mdomasevicius.escrow.orders.Order.State.PENDING;
 
 @Service
 class Orders {
@@ -42,5 +47,16 @@ class Orders {
     @Transactional(readOnly = true)
     public Set<Order> findAll(String user) {
         return repo.findByBuyer(user);
+    }
+
+    @Transactional
+    @Scheduled(fixedDelay = 1000)
+    public void removeUnpaidOrders() {
+        Set<Order> toDelete = repo.findAll()
+                .stream()
+                .filter(o -> o.getCreated().isBefore(now().minusHours(2)) && o.getState() == PENDING)
+                .collect(Collectors.toSet());
+
+        repo.deleteAll(toDelete);
     }
 }
