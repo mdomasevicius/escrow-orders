e8388a69ff6ca3db21fe9716cb61c40fc1cd0592
mdomasevicius <mdomasevicius@gmail.com>
2018-07-11 22:32:22 +0300

    Fix serialization to JSON

---
+++ src/main/java/mdomasevicius/escrow/config/JacksonConfig.java
@@ -0,0 +1,25 @@
+package mdomasevicius.escrow.config;
+
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import static com.fasterxml.jackson.core.JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
+@Configuration
+class JacksonConfig {
+
+    @Bean
+    Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        return Jackson2ObjectMapperBuilder
+                .json()
+                .modules(new Jdk8Module())
+                .modules(new JavaTimeModule())
+                .featuresToDisable(WRITE_DATES_AS_TIMESTAMPS)
+                .featuresToEnable(WRITE_BIGDECIMAL_AS_PLAIN);
+    }
+
+}
